33971997373537025823bb17ccb41743b9786b68
Victoria Kurzweg <vkurzweg@gmail.com>
2018-02-25 16:19:45 -0800

    edit zurich about section

--- app/components/zurich/About/index.js
+++ app/components/zurich/About/index.js
@@ -13,11 +13,23 @@ function About() {
   return (
     <div>
       <Icon type="close" style={{ position: 'absolute', color: 'white', fontSize: '2vw', opacity: '.7', right: '0', paddingTop: '5%', paddingRight: '5%' }} />
-      <Image className='zurich-about-image' cloudName="kurzweg" publicId="empty" alt="sang bleu london" quality="auto" crop="scale" responsive />
+      <h3 className='zurich-about-header'>about sbzh</h3>
+      <Image className='zurich-about-image' cloudName="kurzweg" publicId="table" alt="sang bleu london" quality="auto" crop="scale" responsive />
       <p className='zurich-about-copy'>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
       </p>
-
+      <div className='container-fluid' style={{ marginTop: '5vh' }}>
+        <div className='row' style={{ padding: '0', width: '95%', margin: '0 auto' }}>
+          <div className='col-sm-6 zurich-about-copy zurich-links'>
+            <a href='https://www.instagram.com/sangbleutattoozurich/?hl=en'>instagram</a>
+            <a href='mailto:zurich@sangbleu.tattoo'>zurich@sangbleu.tattoo</a>
+          </div>
+          <div className='col-sm-6 zurich-about-copy zurich-links'>
+            <a href='http://www.sangbleu.tattoo/london'>sang bleu london</a>
+            <a href='http://www.sangbleu.com'>sangbleu.com</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
