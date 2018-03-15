bb8cf984b6c1706e1a9cb286dc53698f21a28dce
Victoria Kurzweg <vkurzweg@gmail.com>
2018-03-14 22:44:59 -0700

    remove old imports

--- app/components/zurich/NamesMobile/index.js
+++ app/components/zurich/NamesMobile/index.js
@@ -6,48 +6,10 @@
 
 import React from 'react';
 // import styled from 'styled-components';
-import ReactHover from 'react-hover';
-import Instafeed from 'react-instafeed';
-import NamesFull from './NamesFull';
-import NamesInitial from './NamesInitial';
-import NamesBlog from './NamesBlog';
-import NamesAbout from './NamesAbout';
 
 
 class InstaHover extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
-  const options = {
-    followCursor:false,
-  }
-  let blogState = this.props.blogOpen;
-  let aboutState = this.props.aboutOpen;
-  let initialState = this.props.initial;
-  let fontShrink;
-  let widthValue;
-  let fullMarginLeft;
-  let fullMarginTop;
-  let fontGrow;
-  let imageClass;
-  let content = <NamesFull />
-  initialState ? widthValue = '70vw' : widthValue;
-  if(blogState){
-    fontShrink = 'font-shrink';
-    widthValue = '40vw';
-    content = <NamesBlog />
-  }
-  if(aboutState){
-    fontShrink = 'font-shrink';
-    widthValue = '45vw';
-    content = <NamesAbout />
-  }
-  if(!initialState && !blogState && !aboutState){
-    widthValue = '98vw';
-    fullMarginLeft = '13vw';
-    fullMarginTop = '1vh';
-    fontGrow = 'font-grow';
-    content = <NamesFull />
-  }
-  initialState ? content = <NamesInitial /> : content;
     return (
       <div style={{ width: '80%', margin: '0 auto', paddingTop: '30vh', overflowY: 'scroll', height: '100%', paddingBottom: '10vh' }}>
         <h2 onClick={this.props.openLightbox.bind(this, '1789183639')} className='name-mobile'>
