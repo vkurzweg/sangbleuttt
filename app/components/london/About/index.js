a4cdc190bde6cefe0b9b6f979ece6cc76f704d2c
Victoria Kurzweg <vkurzweg@gmail.com>
2018-06-30 19:50:25 -0700

    adjust london blog and mobile

--- app/components/london/About/index.js
+++ app/components/london/About/index.js
@@ -11,10 +11,10 @@ import ScrollAnimation from 'react-animate-on-scroll';
 function IntroCopy(props) {
   const studio = 'http://res.cloudinary.com/kurzweg/image/upload/v1519322649/sangbleu/sblondon.png';
   return (
-    <div style={{ paddingTop: '8vh', paddingBottom: '1vh' }}>
+    <div id="about_section" style={{ paddingTop: '8vh', paddingBottom: '1vh' }}>
       <div style={{ marginTop: '2vh', height: '700px', backgroundImage: `url(${studio})`, backgroundAttachment: 'fixed', backgroundSize: 'cover', backgroundPosition: 'center' }} />
       <ScrollAnimation animateIn="fadeIn">
-        <a name="about_section" style={{ color: 'black', textDecoration: 'none' }}><p style={{ fontFamily: 'SuisseRegular', marginLeft: '10vw', letterSpacing: '.02em', fontSize: '1.04vw', lineHeight: '20px', color: 'black', width: '45%', paddingTop: '20vh', paddingBottom: '15vh' }}>
+        <a style={{ color: 'black', textDecoration: 'none' }}><p style={{ fontFamily: 'SuisseRegular', marginLeft: '10vw', letterSpacing: '.02em', fontSize: '1.04vw', lineHeight: '20px', color: 'black', width: '45%', paddingTop: '20vh', paddingBottom: '15vh' }}>
           {props.about}
         </p></a>
       </ScrollAnimation>
